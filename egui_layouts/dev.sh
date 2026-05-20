26a51f86085bafededc757991dacf5b87df7bf54
dxps <marius.ileana@gmail.com>
2026-05-20 23:14:23 +0300

    egui_layouts: 1st commit

---
+++ egui_layouts/dev.sh
@@ -0,0 +1,9 @@
+## Serve the UI in development mode.
+## Notes: 
+##   Use `trunk serve -v --poll` for having the debug level of trunk.
+##   Use `trunk serve -vv --poll` for having the trace level of trunk.
+##   Alternatively, use:
+##       `trunk serve --log-level=debug --poll`.
+##       `trunk serve --log-level=trace --poll`.
+
+trunk serve --poll
